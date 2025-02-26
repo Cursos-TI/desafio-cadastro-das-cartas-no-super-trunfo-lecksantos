7c6fc7c0857d215cd5a0a836c337d8636136f932
lecksantos <lecksantos98@gmail.com>
2025-02-26 20:45:06 +0000

    inserindo operadores matematicos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,7 @@ void cadastro_das_cartas() {
     float pib, area;
 
 
+
     printf("Código da cidade: \n");           //socitando os dados das cartas para armazenar nas variaveis
     scanf ("%s", &codigo);
     
@@ -31,24 +32,34 @@ void cadastro_das_cartas() {
     printf("Área: \n");
     scanf("%f", &area);
 
+    
+
+
+    
+
+
+
 }
 
-//Função para calcular a densidade populacional
-float densidade_populacional() {
-    
-}
+
 
 int main() {
+
+    
+
     printf("***Desafio Super Trunfo*** \n");
 
     printf("Carta 1 \n");
 
     cadastro_das_cartas();
+    float densidade, pib_per_capita;
 
-    
+    densidade = (float) populacao / area;
+    pib_per_capita = (float) pib / populacao;
+
 
     printf("Carta 01: \n");       //exibindo os dados da carta 1
-    printf("Estado: %s\nCódigo da cidade: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmeros de pontos turísticos: %d \n", estado,codigo,nome,populacao,area,pib,pontos_turisticos);
+    printf("Estado: %s\nCódigo da cidade: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmeros de pontos turísticos: %d \n Densidade populacional: %.2f hab/km² \n PIB per capita: %.2f reais\n", estado,codigo,nome,populacao,area,pib,pontos_turisticos,densidade,pib_per_capita);
 
 
     printf("Carta 2 \n");
@@ -57,7 +68,7 @@ int main() {
 
     printf("Carta 02: \n");        //exibindo os dados da carta 2
     printf("Estado: %s\nCódigo da cidade: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmeros de pontos turísticos: %d \n", estado,codigo,nome,populacao,area,pib,pontos_turisticos);
-    
+    printf("Densidade populacional %.2f hab/km² \n PIB per capita: %.2f reais \n", densidade,pib_per_capita);
 
     return 0;
 }
