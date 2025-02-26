5d9ef3d001f71ea5cd54adc7a86d76691ad1ef24
lecksantos <lecksantos98@gmail.com>
2025-02-26 00:00:56 +0000

    adicionando funções

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 
-int main() {
-    printf("Desafio Super Trunfo \n");
-
+//Função para cadastro das cartas
+void cadastro_das_cartas() {
     char estado[2];           //definindo as variaveis
     char codigo[15];
     char nome[30];
     int populacao, pontos_turisticos;
     float pib, area;
 
-    printf("Carta 1 \n");
 
     printf("Código da cidade: \n");           //socitando os dados das cartas para armazenar nas variaveis
-    scanf ("%s", codigo);
+    scanf ("%s", &codigo);
     
     printf("Estado: \n");
-    scanf("%s", estado);
+    scanf("%s", &estado);
 
     printf("Nome da cidade: \n");
-    scanf("%s", nome);
+    scanf("%s", &nome);
 
     printf("População: \n");
     scanf("%d", &populacao);
@@ -32,6 +30,22 @@ int main() {
 
     printf("Área: \n");
     scanf("%f", &area);
+
+}
+
+//Função para calcular a densidade populacional
+float densidade_populacional() {
+    
+}
+
+int main() {
+    printf("***Desafio Super Trunfo*** \n");
+
+    printf("Carta 1 \n");
+
+    cadastro_das_cartas();
+
+    
 
     printf("Carta 01: \n");       //exibindo os dados da carta 1
     printf("Estado: %s\nCódigo da cidade: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmeros de pontos turísticos: %d \n", estado,codigo,nome,populacao,area,pib,pontos_turisticos);
@@ -39,26 +53,7 @@ int main() {
 
     printf("Carta 2 \n");
 
-    printf("Código da cidade: \n");
-    scanf ("%s", codigo);
-    
-    printf("Estado: \n");
-    scanf("%s", estado);
-
-    printf("Nome da cidade: \n");
-    scanf("%s", nome);
-
-    printf("População: \n");
-    scanf("%d", &populacao);
-
-    printf("Números de pontos turísticos: \n");
-    scanf("%d", &pontos_turisticos);
-
-    printf("PIB: \n");
-    scanf("%f", &pib);
-
-    printf("Área: \n");
-    scanf("%f", &area);
+    cadastro_das_cartas();
 
     printf("Carta 02: \n");        //exibindo os dados da carta 2
     printf("Estado: %s\nCódigo da cidade: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmeros de pontos turísticos: %d \n", estado,codigo,nome,populacao,area,pib,pontos_turisticos);
